9c7b559fd86b281300a83e8bf40fd929d24599bc
andrei <andreimd02@gmail.com>
2020-06-18 12:23:20 -0300

    init

---
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from '../curso';
+import { Observable, empty, of, Subject, EMPTY } from 'rxjs';
+import { catchError, switchMap, take } from 'rxjs/operators';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.component';
+import { AlertModalService } from '../../shared/alert-modal.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Cursos2Service } from '../cursos2.service';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
+})
+export class CursosListaComponent implements OnInit {
+
+  deleteModalRef: BsModalRef;
+  @ViewChild('deleteModal', { static: true }) deleteModal;
+
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
+
+  cursoSelecionado: Curso;
+
+  constructor(
+    private service: Cursos2Service,
+    private modalService: BsModalService,
+    private alertService: AlertModalService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list().pipe(
+      // map(),
+      // tap(),
+      // switchMap(),
+      catchError(error => {
+        console.error(error);
+        this.handleError();
+        return empty();
+      })
+    );
+  }
+
+  handleError() {
+    this.alertService.showAlertDanger('Erro ao carregar cursos. Tente novamente mais tarde.');
+  }
+
+  onEdit(id) {
+    this.router.navigate(['editar', id], { relativeTo: this.route });
+  }
+
+  onDelete(curso) {
+    this.cursoSelecionado = curso;
+
+    const result$ = this.alertService.showConfirm('Confirmacao', 'Tem certeza que deseja remover esse curso?');
+    result$.asObservable()
+    .pipe(
+      take(1),
+      switchMap(result => result ? this.service.remove(curso.id) : EMPTY)
+    )
+    .subscribe(
+      success => {
+        this.onRefresh();
+      },
+      error => {
+        this.alertService.showAlertDanger('Erro ao remover curso. Tente novamente mais tarde.');
+      }
+    );
+  }
+
+  onConfirmDelete() {
+    this.service.remove(this.cursoSelecionado.id)
+    .subscribe(
+      success => {
+        this.onRefresh();
+        this.deleteModalRef.hide();
+      },
+      error => {
+        this.alertService.showAlertDanger('Erro ao remover curso. Tente novamente mais tarde.');
+        this.deleteModalRef.hide();
+      }
+    );
+  }
+
+  onDeclineDelete() {
+    this.deleteModalRef.hide();
+  }
+}
---
+++ rotas/src/app/app.routing.module.ts
@@ -0,0 +1,34 @@
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { CursosGuard } from './guards/cursos.guard';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes = [
+    { path: 'cursos',
+        loadChildren: 'app/cursos/cursos.module#CursosModule',
+        canActivate: [AuthGuard],
+        canActivateChild: [CursosGuard],
+        canLoad: [AuthGuard]
+    },
+    { path: 'alunos',
+        loadChildren: 'app/alunos/alunos.module#AlunosModule',
+        canActivate: [AuthGuard],
+        canLoad: [AuthGuard]
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PaginaNaoEncontradaComponent } //, canActivate: [AuthGuard]}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
